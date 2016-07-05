1b907e06f855874daf3c2ae671893ef7147df9a7
MikimotoH <MikimotoH@gmail.com>
2016-07-05 11:31:36 +0000

    fetch guest_ip, netdev from PSQL image table, totally independent from ./scratch/774/run.sh

--- scripts/test_network_reachable.py
+++ scripts/test_network_reachable.py
@@ -5,17 +5,20 @@ import sys
 import pdb
 import traceback
 import psycopg2
+import re
+import time
+from urllib import request
+import subprocess
 
 conn = psycopg2.connect(database="firmware", user="firmadyne", 
         password="firmadyne", host="127.0.0.1")
 cur = conn.cursor()
 
-firmadyne_root = path.dirname(path.dirname(path.realpath(__file__)))
-FIRMWARE_DIR='/home/mikil/firmadyne/firmadyne/'
-BINARY_DIR='{FIRMWARE_DIR}/binaries/'.format(**globals())
-TARBALL_DIR='{FIRMWARE_DIR}/images/'.format(**globals())
-SCRATCH_DIR='{FIRMWARE_DIR}/scratch/'.format(**globals())
-SCRIPT_DIR='{FIRMWARE_DIR}/scripts/'.format(**globals())
+FIRMWARE_DIR = path.dirname(path.dirname(path.realpath(__file__)))
+BINARY_DIR=path.join(FIRMWARE_DIR,'binaries')
+TARBALL_DIR=path.join(FIRMWARE_DIR,'images')
+SCRATCH_DIR=path.join(FIRMWARE_DIR,'scratch')
+SCRIPT_DIR=path.join(FIRMWARE_DIR,'scripts')
 
 def gl(localvars):
     d=globals()
@@ -23,17 +26,20 @@ def gl(localvars):
     return d
 
 def shell(cmd):
-    import subprocess
     cmd = path.expandvars(cmd)
     proc= subprocess.Popen(cmd, shell=True,stdout=subprocess.PIPE)
     returncode = proc.wait()
     cmdout = proc.stdout.read().decode('utf8')
     print(cmdout, end='', flush=True)
+    if returncode!=0:
+        print('\'%s\' returns %d'%(cmd,returncode), file=sys.stderr)
     return returncode, cmdout
 
 def get_kernel(archend):
-    if archend=='mipsel':
-        return "{BINARY_DIR}/vmlinux.{archend}".format(**gl(locals()))
+    if archend=='armel':
+        return path.join(BINARY_DIR,'zImage.%s'%archend)
+    elif archend=='mipsel' or archend=='mipseb':
+        return path.join(BINARY_DIR,'vmlinux.%s'%archend)
 def get_qemu_machine(archend):
     if archend=='armel':
         return 'virt'
@@ -52,9 +58,18 @@ def get_qemu(archend):
     elif archend=='mipsel':
         return 'qemu-system-mipsel'
 def get_scratch(iid):
-    return ('{SCRATCH_DIR}/{iid}'.format(**gl(locals())))
+    return path.join(SCRATCH_DIR, '%d'%iid)
 def get_fs(iid):
-    return (get_scratch(iid)+'/image.raw')
+    return path.join(get_scratch(iid),'image.raw')
+def closeIp(ip):
+    tups = [int(x) for x in ip.split(".")]
+    if tups[3] != 1:
+        tups[3] -= 1
+    else:
+        tups[3] = 2
+    return ".".join([str(x) for x in tups])
+
+
 
 if len(sys.argv)<2:
     print("usage: \n\
@@ -63,11 +78,10 @@ if len(sys.argv)<2:
 IID = int(sys.argv[1])
 cur.execute("SELECT arch FROM image WHERE id=%s"%IID)
 ARCHEND = cur.fetchone()[0]
-# ARCHEND='mipsel'
-# IID=774
-GUESTIP='192.168.1.220'
-NETDEVIP='192.168.1.219'
-TAPDEV='tap%s'%IID
+cur.execute("SELECT guest_ip FROM image WHERE id=%d"%IID)
+GUESTIP = cur.fetchone()[0]
+NETDEVIP=closeIp(GUESTIP)
+TAPDEV='tap%d'%IID
 HOSTNETDEV=TAPDEV
 
 IMAGE=get_fs(IID)
@@ -84,45 +98,60 @@ shell('sudo ifconfig {HOSTNETDEV} {NETDEVIP}/24 up'.format(**gl(locals())))
 print("Adding route to {GUESTIP}...".format(**gl(locals())))
 shell('sudo route add -host {GUESTIP} gw {GUESTIP} {HOSTNETDEV}'.format(**gl(locals())))
 
-print("Starting emulation of firmware... ")
-shell('''{QEMU} -m 256 -M {QEMU_MACHINE} -kernel {KERNEL} \
-        -drive if=ide,format=raw,file={IMAGE} -append "root={QEMU_ROOTFS} console=ttyS0 nandsim.parts=64,64,64,64,64,64,64,64,64,64 rdinit=/preInit.sh rw debug ignore_loglevel print-fatal-signals=1 user_debug=31 firmadyne.syscall=8" \
-    -serial file:{WORK_DIR}/qemu.final.serial.log \
-    -serial unix:/tmp/qemu.{IID}.S1,server,nowait \
-    -monitor unix:/tmp/qemu.{IID},server,nowait \
-    -display none \
-    -daemonize \
-    -net nic,vlan=0 -net socket,vlan=0,listen=:2000 -net nic,vlan=1 -net tap,vlan=1,ifname={TAPDEV},script=no -net nic,vlan=2 -net socket,vlan=2,listen=:2002 -net nic,vlan=3 -net socket,vlan=3,listen=:2003  \
-    '''.format(**gl(locals())))
+def ifaceNo(dev):
+    g = re.match(r"[^0-9]+([0-9]+)", dev)
+    return int(g.group(1))
+def qemuNetworkConfig(dev):
+    result = ""
+    for i in range(0, 4):
+        if i == ifaceNo(dev):
+            result += "-net nic,vlan=%i -net tap,vlan=%i,ifname={TAPDEV},script=no ".format(**gl(locals())) \
+                    % (i, i)
+        else:
+            result += "-net nic,vlan=%i -net socket,vlan=%i,listen=:200%i "% (i, i, i)
+    return result
 
-import time
+print("Starting emulation of firmware... ")
+if ARCHEND=='mipsel' or ARCHEND=='mipseb':
+    qemuEnvVars=""
+    qemuDisk = "-drive if=ide,format=raw,file={IMAGE}".format(**locals())
+elif ARCHEND=='armel':
+    qemuEnvVars = "QEMU_AUDIO_DRV=none "
+    qemuDisk = "-drive if=none,file={IMAGE},format=raw,id=rootfs -device virtio-blk-device,drive=rootfs".format(**locals())
+cur.execute("SELECT netdev FROM image WHERE id=%d"%IID)
+netdev=cur.fetchone()[0]
+qemuNetwork=qemuNetworkConfig(netdev)
+
+
+shell('''{qemuEnvVars} {QEMU} -m 256 -M {QEMU_MACHINE} -kernel {KERNEL} \
+        {qemuDisk} -append "root={QEMU_ROOTFS} console=ttyS0 \
+        nandsim.parts=64,64,64,64,64,64,64,64,64,64 rdinit=/preInit.sh rw debug ignore_loglevel \
+        print-fatal-signals=1 user_debug=31 firmadyne.syscall=8" \
+        -serial file:{WORK_DIR}/qemu.final.serial.log \
+        -display none \
+        -daemonize \
+        {qemuNetwork}'''.format(**gl(locals())))
+
+# the good sleep time depends on the time whether eth1 appear in the serial.log
 time.sleep(10)
-from urllib import request
 network_reachable=False
 try:
     with request.urlopen('http://{GUESTIP}/'.format(**gl(locals())), timeout=10) as fin:
         htmlsrc = fin.readall().decode('utf8')
         if len(htmlsrc)>0:
-            print('network_reachable')
             network_reachable=True
-        else:
-            print('network not reachable')
 except Exception as ex:
-    print('network not reachable')
-    # traceback.print_exc()
+    pass
+print('network_reachable=%s'%network_reachable)
 cur.execute("UPDATE image SET network_reachable=%s WHERE id=%s", 
         (network_reachable, IID))
 conn.commit()
 
 
-print("Done!")
-print("The emulated firmware may not be accessible while booting.")
-print("Press any key to destroy the network and shutdown emulation.")
-input()
 shell('killall {QEMU}'.format(**gl(locals())))
 
 print( "Deleting route...")
-shell('sudo route del -host {GUESTIP} gw {GUESTIP} ${HOSTNETDEV}'.format(**gl(locals())))
+shell('sudo route del -host {GUESTIP} gw {GUESTIP} {HOSTNETDEV}'.format(**gl(locals())))
 
 print( "Bringing down TAP device...")
 shell('sudo ifconfig {TAPDEV} down'.format(**gl(locals())))
