eadf52157b0072af837f07d6fca4970c318cccb4
MikimotoH <MikimotoH@gmail.com>
2016-07-15 03:49:33 +0000

    add more checks before running, more verbose error message

--- scripts/process_firmware_file.py
+++ scripts/process_firmware_file.py
@@ -5,6 +5,7 @@ import argparse
 import sys
 import re
 from os import path
+from psql_firmware import psql
 
 
 parser = argparse.ArgumentParser()
@@ -24,26 +25,40 @@ if ret!=0:
     sys.exit(ret)
 # >> Database Image ID: 774
 iid =  int(re.search(r'Image ID: (\d+)', txt).group(1))
+if not path.exists('images/%(iid)d.tar.gz'):
+    print('failed to extract "%(fw_file)s" ', file=sys.stderr)
+    sys.exit()
+
 ret, txt = shell('./scripts/getArch.sh ./images/%(iid)d.tar.gz 2>&1'%locals())
 if ret!=0:
+    print('failed to getArch.sh %(iid)s'%locals(), file=sys.stderr)
     sys.exit(ret)
-arch = txt.split()[-1]
-assert arch in ['mipsel','mipseb','armel']
+arch = psql("SELECT arch FROM image WHERE id=%d"%iid)
+if arch not in ['mipsel','mipseb','armel']:
+    print('unknown CPU Architect "%(arch)s" for %(iid)d'%locals(), file=sys.stderr)
+    sys.exit(1)
+
 ret, txt = shell('./scripts/tar2db.py -i %(iid)d -f images/%(iid)d.tar.gz 2>&1'%locals())
 if ret!=0:
     if 'duplicate key value' not in  txt:
+        print('failed to tar2db.py -i %(iid)d -f "images/%(iid)d.tar.gz"'%locals(), file=sys.stderr)
         sys.exit(ret)
+
 ret, txt = shell('sudo ./scripts/makeImage.sh %(iid)d %(arch)s'%locals())
-# if ret!=0:
-#    sys.exit(ret)
+shell('sudo rm -f images/%(iid)d.tar.gz'%locals())
 if not path.exists('./scratch/%(iid)d/image.raw'%locals()):
+    print('failed to makeImage for %(iid)d'%locals(), file=sys.stderr)
     sys.exit(ret)
+
 print('inferNetwork.sh %(iid)s %(arch)s'%locals())
 ret, txt = shell('./scripts/inferNetwork.sh %(iid)s %(arch)s'%locals())
 if 'network_inferred=true' not in txt:
+    print('failed to inferNetwork.sh %(iid)d %(arch)s'%locals(), file=sys.stderr)
     sys.exit(ret)
+
 print('test_network_reachable')
 from test_network_reachable import test_network_reachable
 test_network_reachable(iid)
 # ret ,txt= shell('./scripts/run.sh %(iid)s'%locals())
 # shell('./analyses/runExploits.py -t %(guest_ip)s'%locals())
+
