8b19cd466ae0550cf1b063be59e6882cadeef5bf
MikimotoH <MikimotoH@gmail.com>
2016-09-23 08:19:01 +0000

    use fw_file_to_psql.py --brand

--- scripts/process_firmware_file.sh
+++ scripts/process_firmware_file.sh
@@ -6,12 +6,10 @@ brand=$1
 fw_file=$2
 
 ## IID=$(sources/extractor/extractor.py -b "${brand}" -sql 127.0.0.1 -np -nk "${fw_file}" images | tee /dev/tty | grep  -E 'Database Image ID:' | grep -o -E '[0-9]+')
-sources/extractor/extractor.py -b "${brand}" -sql 127.0.0.1 -np -nk "${fw_file}" images
-IID=$(scripts/psql_firmware.py "SELECT MAX(id) FROM image")
-extract_OK=$(scripts/psql_firmware.py "SELECT rootfs_extracted FROM image WHERE id=${IID};")
-if [ "$extract_OK" == "False" ] ; then
-    exit 0
-fi
+python -u sources/extractor/extractor.py -b "${brand}" -sql 127.0.0.1 -np -nk "${fw_file}" images | tee extraction.log
+IID=`cat extraction.log | sed -r 's/.*Database Image ID: ([0-9]+)/\1/;tx;d;:x'`
+[ -e images/$IID.tar.gz ] || { echo "images/$IID.tar.gz doesn't exist. Extraction failed."; exit 1 }
+scripts/fw_files_to_psql.py $fw_file --brand $brand
 
 scripts/getArch.sh images/${IID}.tar.gz
 arch=$(scripts/psql_firmware.py "SELECT arch FROM image WHERE id=${IID};")
