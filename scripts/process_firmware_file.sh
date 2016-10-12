9dff4d98c3fe4e5fda577fd8ff75838320e8370b
MikimotoH <MikimotoH@gmail.com>
2016-10-12 08:04:00 +0000

    fix {;;} if images/${iid}.tar.gz not exist

--- scripts/process_firmware_file.sh
+++ scripts/process_firmware_file.sh
@@ -8,8 +8,10 @@ fw_file=$2
 ## IID=$(sources/extractor/extractor.py -b "${brand}" -sql 127.0.0.1 -np -nk "${fw_file}" images | tee /dev/tty | grep  -E 'Database Image ID:' | grep -o -E '[0-9]+')
 python -u sources/extractor/extractor.py -b "${brand}" -sql 127.0.0.1 -np -nk "${fw_file}" images | tee extraction.log
 IID=`cat extraction.log | sed -r 's/.*Database Image ID: ([0-9]+)/\1/;tx;d;:x'`
-[ -e images/$IID.tar.gz ] || { echo "images/$IID.tar.gz doesn't exist. Extraction failed."; exit 1 }
-scripts/fw_files_to_psql.py $fw_file --brand $brand
+echo "\$IID = $IID"
+echo "scripts/fw_file_to_psql.py $fw_file --brand $brand"
+scripts/fw_file_to_psql.py $fw_file --brand $brand
+[ -e images/${IID}.tar.gz ] || { echo "images/${IID}.tar.gz doesn't exist. Extraction failed."; exit 1; }
 
 scripts/getArch.sh images/${IID}.tar.gz
 arch=$(scripts/psql_firmware.py "SELECT arch FROM image WHERE id=${IID};")
@@ -23,3 +25,4 @@ if [ "$net_infer_OK" == "False" ] ; then
     exit 0
 fi
 scripts/test_network_reachable.py ${IID}
+
