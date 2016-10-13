0afd78d849601b79b46dfeb189f382a717a9ea56
MikimotoH <MikimotoH@gmail.com>
2016-10-13 07:55:46 +0000

    add 4 purpose: test/emulate/construct/destruct Use test to test network reachability in automatic process. Use emulate to test network in a manually way. Use construct if you wanna use metasploit in the same process. Use destruct to destruct network setups.

--- scripts/test_network_reachable.py
+++ scripts/test_network_reachable.py
@@ -29,16 +29,22 @@ def get_kernel(archend):
         return path.join(BINARY_DIR,'zImage.%s'%archend)
     elif archend=='mipsel' or archend=='mipseb':
         return path.join(BINARY_DIR,'vmlinux.%s'%archend)
+
+
 def get_qemu_machine(archend):
     if archend=='armel':
         return 'virt'
     elif archend=='mipseb' or archend=='mipsel':
         return 'malta'
+
+
 def get_qemu_disk(archend):
     if archend=='armel':
         return '/dev/vda1'
     elif archend=='mipseb' or archend=='mipsel':
         return '/dev/sda1'
+
+
 def get_qemu(archend):
     if archend=='armel':
         return 'qemu-system-arm'
@@ -46,10 +52,16 @@ def get_qemu(archend):
         return 'qemu-system-mips'
     elif archend=='mipsel':
         return 'qemu-system-mipsel'
+
+
 def get_scratch(iid):
     return path.join(SCRATCH_DIR, '%d'%iid)
+
+
 def get_fs(iid):
     return path.join(get_scratch(iid),'image.raw')
+
+
 def closeIp(ip):
     tups = [int(x) for x in ip.split(".")]
     if tups[3] != 1:
@@ -58,9 +70,11 @@ def closeIp(ip):
         tups[3] = 2
     return ".".join([str(x) for x in tups])
 
+
 def ifaceNo(dev):
     g = re.match(r"[^0-9]+([0-9]+)", dev)
     return int(g.group(1))
+
 
 def qemuNetworkConfig(netdev, iid):
     tapdev='tap%d'%iid
@@ -71,6 +85,7 @@ def qemuNetworkConfig(netdev, iid):
         else:
             result += "-net nic,vlan=%(i)d -net socket,vlan=%(i)d,listen=:200%(i)d "% locals()
     return result
+
 
 def get_qemu_cmd_line(iid, archend):
     IMAGE=get_fs(iid)
@@ -100,7 +115,8 @@ def get_qemu_cmd_line(iid, archend):
             -daemonize \
             {qemuNetwork}'''.format(**locals()))
 
-def try_ip(ip_addr, loopcount=20, timeout=3):
+
+def try_ip(ip_addr, loopcount=3, timeout=20):
     for _i in range(loopcount):
         try:
             print('test http://%s/'%ip_addr)
@@ -110,74 +126,80 @@ def try_ip(ip_addr, loopcount=20, timeout=3):
             pass
     return False
 
-def test_network_reachable(iid, emulate_only=False):
+
+def construct(iid):
     archend = psql("SELECT arch FROM image WHERE id=%d"%iid)
-    if archend not in ['mipseb', 'mipsel', 'armel']:
-        print('archend="%(archend)s" is invalid!'%locals())
-        return
     guestip = psql("SELECT guest_ip FROM image WHERE id=%d"%iid)
-    if not guestip:
-        print('guestip is empty!')
-        return
     netdev = psql("SELECT netdev FROM image WHERE id=%d"%iid)
-    if not netdev:
-        print('netdev is empty!')
-        return
     netdevip=closeIp(guestip)
     tapdev='tap%d'%iid
-    hostnetdev=tapdev
+    print("Creating TAP device %(tapdev)s..."%locals())
+    shell('sudo tunctl -t %(tapdev)s -u $USER'%locals())
+    print("Bringing up TAP device...")
+    shell('sudo ifconfig %(tapdev)s %(netdevip)s/24 up'%locals())
+    print("Adding route to %(guestip)s..."%locals())
+    shell('sudo route add -host %(guestip)s gw %(guestip)s %(tapdev)s'%locals())
+    print("Starting emulation of firmware... ")
+    WORK_DIR=get_scratch(iid)
+    shell('sudo rm -f {WORK_DIR}/qemu.final.serial.log'.format(**locals()))
+    ret, _ = shell(get_qemu_cmd_line(iid, archend))
+    if ret!=0:
+        print('failed to launch %s'%get_qemu(archend), file=sys.stderr)
+        return False
+    return True
 
-    try:
-        print("Creating TAP device %(tapdev)s..."%locals())
-        shell('sudo tunctl -t %(tapdev)s -u $USER'%locals())
-        print("Bringing up TAP device...")
-        shell('sudo ifconfig %(hostnetdev)s %(netdevip)s/24 up'%locals())
-        print("Adding route to %(guestip)s..."%locals())
-        shell('sudo route add -host %(guestip)s gw %(guestip)s %(hostnetdev)s'%locals())
-        print("Starting emulation of firmware... ")
-        WORK_DIR=get_scratch(iid)
-        shell('sudo rm -f {WORK_DIR}/qemu.final.serial.log'.format(**locals()))
-        ret, _ = shell(get_qemu_cmd_line(iid, archend))
-        if ret!=0:
-            print('failed to launch %s'%get_qemu(archend), file=sys.stderr)
-            raise Exception(ret)
-        if emulate_only==False:
-            time.sleep(10)
-            network_reachable = try_ip(guestip, 20, 3)
-            print('network_reachable=%s'%network_reachable)
-            psql("UPDATE image SET network_reachable=%s WHERE id=%s", (network_reachable, iid))
-            print("Done!")
-        else:
-            print('open http://%(guestip)s/'%locals())
-            print('press any key to stop')
-            input()
-    except Exception as ex:
-        pass
-    finally:
-        QEMU=get_qemu(archend)
-        shell('killall %(QEMU)s'%locals())
-        print( "Deleting route...")
-        shell('sudo route del -host %(guestip)s gw %(guestip)s %(hostnetdev)s'%locals())
-        print( "Bringing down %(tapdev)s..."%locals())
-        shell('sudo ifconfig %(tapdev)s down'%locals())
-        print("Deleting TAP device %(tapdev)s... "%locals())
-        shell('sudo tunctl -d %(tapdev)s'%locals())
+
+def destruct(iid):
+    archend = psql("SELECT arch FROM image WHERE id=%d"%iid)
+    guestip = psql("SELECT guest_ip FROM image WHERE id=%d"%iid)
+    netdev = psql("SELECT netdev FROM image WHERE id=%d"%iid)
+    tapdev='tap%d'%iid
+    QEMU=get_qemu(archend)
+    shell('killall %(QEMU)s'%locals())
+    print( "Deleting route...")
+    shell('sudo route del -host %(guestip)s gw %(guestip)s %(tapdev)s'%locals())
+    print( "Bringing down %(tapdev)s..."%locals())
+    shell('sudo ifconfig %(tapdev)s down'%locals())
+    print("Deleting TAP device %(tapdev)s... "%locals())
+    shell('sudo tunctl -d %(tapdev)s'%locals())
+    return True
 
 
 def main():
     if len(sys.argv)<2:
-        print("usage: \n\
-                %s <IID> [emulate]"%(sys.argv[0]))
+        print("""
+        usage: 
+                {0} <IID> [test] 
+                {0} <IID> [emulate] 
+                {0} <IID> [construct]
+                {0} <IID> [destruct]
+                """.format(sys.argv[0]))
         return
     iid = int(sys.argv[1])
-    emulate_only=False
-    if len(sys.argv)>2:
-        emulate_only = sys.argv[2]=='emulate'
-    if emulate_only==False:
-        print('test_network_reachable %(iid)d'%locals())
-    else:
-        print('emulate image iid=%(iid)d'%locals())
-    test_network_reachable(iid, emulate_only)
+    purpose = sys.argv[2] if len(sys.argv)>2 else "test"
+    if purpose=='test':
+        if not construct(iid):
+            destruct(iid)
+            return False
+        guestip = psql("SELECT guest_ip FROM image WHERE id=%d"%iid)
+        time.sleep(10)
+        network_reachable = try_ip(guestip)
+        print('network_reachable=%s'%network_reachable)
+        psql("UPDATE image SET network_reachable=%s WHERE id=%s", (network_reachable, iid))
+        return destruct(iid)
+    elif purpose=='emulate':
+        if not construct(iid):
+            destruct(iid)
+            return False
+        guestip = psql("SELECT guest_ip FROM image WHERE id=%d"%iid)
+        print('open http://%(guestip)s/'%locals())
+        print('press any key to stop')
+        input()
+        return destruct(iid)
+    elif purpose=='construct':
+        return construct(iid)
+    elif purpose=='destruct':
+        return destruct(iid)
 
 
 if __name__=="__main__":
