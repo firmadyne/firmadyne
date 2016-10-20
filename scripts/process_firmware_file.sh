c3ac64431e2a5d19bc1ddedf58ef4ff97f77d85a
MikimotoH <MikimotoH@gmail.com>
2016-10-20 16:09:09 +0000

    net_reachable typo, quote "fw_file" arg, add merge_metasploit_log.py

--- scripts/process_firmware_file.sh
+++ scripts/process_firmware_file.sh
@@ -9,8 +9,8 @@ python -u sources/extractor/extractor.py -b "${brand}" -sql 127.0.0.1 -np -nk "$
 IID=`cat extraction.log | sed -r 's/.*Database Image ID: ([0-9]+)/\1/;tx;d;:x'`
 rm extraction.log
 echo "\$IID = $IID"
-echo "scripts/fw_file_to_psql.py $fw_file --brand $brand"
-scripts/fw_file_to_psql.py $fw_file --brand $brand
+echo "scripts/fw_file_to_psql.py \"$fw_file\" --brand $brand"
+scripts/fw_file_to_psql.py "$fw_file" --brand $brand
 [ -e images/${IID}.tar.gz ] || { echo "images/${IID}.tar.gz doesn't exist. Extraction failed."; exit 1; }
 
 scripts/getArch.sh images/${IID}.tar.gz
@@ -26,12 +26,15 @@ if [ "$net_infer_OK" == "False" ] ; then
 fi
 # net_reachable
 python3 -u scripts/test_network_reachable.py ${IID} test | tee test_network_reachable.log
-net_reachalbe=$(cat test_network_reachable.log | grep "network_reachable=" | grep -ohE 'True|False')
+net_reachable=$(cat test_network_reachable.log | grep "network_reachable=" | grep -ohE 'True|False')
 rm test_network_reachable.log
 if [ "$net_reachable" == "False" ] ; then
     exit 0
 fi
-python3 -u scripts/test_network_reachable.py ${IID} construct
-python3 -u analyses/runExploits.py -i ${IID}
-python3 -u scripts/test_network_reachable.py ${IID} destruct
+guest_ip=$(scripts/psql_firmware.py "SELECT guest_ip FROM image WHER id=${IID}")
+scripts/test_network_reachable.py ${IID} construct
+while ! ping -c1 $guest_ip &>/dev/null; do :; done
+analyses/runExploits.py -i ${IID}
+scripts/test_network_reachable.py ${IID} destruct
+scripts/merge_metasploit_logs.py ${IID}
 
