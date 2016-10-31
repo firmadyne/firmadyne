0804b8696697abb5842fa9170239f71f7b279f53
MikimotoH <MikimotoH@gmail.com>
2016-10-31 21:35:38 +0000

    use scripts/extractor.py instead, add explains

--- scripts/process_firmware_file.sh
+++ scripts/process_firmware_file.sh
@@ -5,7 +5,8 @@ set -u
 brand=$1
 fw_file=$2
 
-python -u sources/extractor/extractor.py -b "${brand}" -sql 127.0.0.1 -np -nk "${fw_file}" images | tee extraction.log
+echo "<<1>> extract fw_file\n"
+python -u scripts/extractor.py -b "${brand}" "${fw_file}" images | tee extraction.log
 IID=`cat extraction.log | sed -r 's/.*Database Image ID: ([0-9]+)/\1/;tx;d;:x'`
 rm extraction.log
 echo "\$IID = $IID"
@@ -13,6 +14,7 @@ echo "scripts/fw_file_to_psql.py \"$fw_file\" --brand $brand"
 scripts/fw_file_to_psql.py "$fw_file" --brand $brand
 [ -e images/${IID}.tar.gz ] || { echo "images/${IID}.tar.gz doesn't exist. Extraction failed."; exit 1; }
 
+echo "<<2>> store unpacked file names and hashes\n"
 scripts/getArch.sh images/${IID}.tar.gz
 arch=$(scripts/psql_firmware.py "SELECT arch FROM image WHERE id=${IID};")
 scripts/tar2db_tlsh.py images/${IID}.tar.gz
@@ -20,6 +22,7 @@ sudo ./scripts/makeImage.sh ${IID} $arch
 sudo chown -R $USER:$USER scratch/${IID}
 
 # infer network
+echo "<<2>> infer network config\n"
 echo "inferNetwork.sh ${IID}"
 scripts/inferNetwork.sh ${IID} $arch
 net_infer_OK=$(scripts/psql_firmware.py "SELECT network_inferred FROM image WHERE id=${IID};")
@@ -27,6 +30,7 @@ if [ "$net_infer_OK" != "True" ] ; then
     echo "network inferred = False"
     exit 0
 fi
+echo "<<4>> test network_reachable\n"
 # net_reachable
 python3 -u scripts/test_network_reachable.py ${IID} test | tee test_network_reachable.log
 net_reachable=$(cat test_network_reachable.log | grep "network_reachable=" | grep -ohE 'True|False')
@@ -35,6 +39,7 @@ if [ "$net_reachable" != "True" ] ; then
     echo "network_reachable = False"
     exit 0
 fi
+echo "<<5>> Metasploit and Nmap scan\n"
 guest_ip=$(scripts/psql_firmware.py "SELECT guest_ip FROM image WHERE id=${IID}")
 scripts/test_network_reachable.py ${IID} construct
 while ! ping -c1 $guest_ip &>/dev/null; do :; done
