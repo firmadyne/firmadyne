343c0fed3b8729cb6151ede94000012fe7719565
MikimotoH <MikimotoH@gmail.com>
2016-12-26 04:51:48 +0000

    change to line based

--- scripts/shellutils.py
+++ scripts/shellutils.py
@@ -7,12 +7,15 @@ import sys
 def shell(cmd):
     bufsize=8
     cmd = path.expandvars(cmd)
-    proc= subprocess.Popen(cmd, shell=True,stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    proc= subprocess.Popen(cmd, shell=True,bufsize=1, 
+            stdout=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines=True)
     ret=None
     cmdout=''
     while True:
-        s = proc.stdout.read(bufsize).decode('utf8')
-        print(s, end='', flush=True)
+        s = proc.stdout.readline()
+        print(s, flush=True)
+        # s = proc.stdout.read(bufsize).decode('utf8')
+        # print(s, end='', flush=True)
         cmdout+=s
         s = proc.stderr.read(bufsize).decode('utf8')
         print(s, end='', flush=True)
