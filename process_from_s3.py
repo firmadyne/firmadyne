aaca3257f8483e98601996b40d6f06c3b653e534
MikimotoH <MikimotoH@gmail.com>
2017-02-23 07:23:27 +0000

    sleep before and after sync in makeImage.sh

--- process_from_s3.py
+++ process_from_s3.py
@@ -21,9 +21,9 @@ def main():
     try:
         conn = boto.connect_s3()
         buck = conn.get_bucket('grid-iot-firmware-harvest')
-        for obj in buck.list('fw_files/D-Link/'):
+        for obj in buck.list('fw_files/Zyxel/'):
             md5 = getBucketMd5(buck, obj)
-            psql("UPDATE image SET brand ='D-Link' WHERE hash=%(md5)s", locals())
+            psql("UPDATE image SET brand ='Zyxel' WHERE hash=%(md5)s", locals())
             idlist= psql("SELECT id FROM image WHERE hash=%(md5)s and open_ports_ts is not NULL LIMIT 1", locals())
             if bool(idlist):
                 # print('Already processed "%(fname)s"' % locals())
@@ -33,7 +33,7 @@ def main():
             obj.get_contents_to_filename(fname)
             begin = time.time()
             print('begin=%s' % datetime.fromtimestamp(begin))
-            os.system('python3 -u scripts/process_firmware_file.py "D-Link" "%s"'%fname)
+            os.system('python3 -u scripts/process_firmware_file.py "Zyxel" "%s"'%fname)
             os.remove(fname)
             end = time.time()
             print('end=%s' % datetime.fromtimestamp(end))
